241fe3c759391a558d5aa536e577faaaf14dd593
ksyv <ksyviais@gmail.com>
2025-06-18 11:27:37 +0200

    Config editor for fix linter

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,64 +1,64 @@
 import redis from 'redis';
 
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
-        this.connected = false;
-        this.client.on("connect", () => {
-            this.connected = true;
-            console.log("Connected to Redis");
-        });
-        this.client.on("error", (err) => {
-            console.error(`Redis error: ${err.message}`);
-            this.connected = false;
-        });
-        this.client.on("end", () => {
-            console.log("Redis connection closed");
-            this.connected = false;
-        });
-    }
+  constructor() {
+    this.client = redis.createClient();
+    this.connected = false;
+    this.client.on("connect", () => {
+      this.connected = true;
+      console.log("Connected to Redis");
+    });
+    this.client.on("error", (err) => {
+      console.error(`Redis error: ${err.message}`);
+      this.connected = false;
+    });
+    this.client.on("end", () => {
+      console.log("Redis connection closed");
+      this.connected = false;
+    });
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
+  async get(key) {
 
-        return new Promise((resolve, reject) => {
-            this.client.get(key, (err, value) => {
-                if (err) {
-                    console.error(`Error getting key ${key}: ${err.message}`);
-                    return reject(err);
-                }
-                resolve(value);
-            });
-        });
-    }
-
-    async set(key, value, duration) {
-        return new Promise((resolve, reject) => {
-            this.client.set(key, value, 'EX', duration, (err, reply) => {
-                if (err) {
-                    console.error(`Error setting key ${key}: ${err.message}`);
-                    return reject(err);
-                }
-                resolve(reply);
-            });
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) {
+          console.error(`Error getting key ${key}: ${err.message}`);
+          return reject(err);
         }
-        );
-    }
+        resolve(value);
+      });
+    });
+  }
 
-    async del(key) {
-        return new Promise((resolve, reject) => {
-            this.client.del(key, (err, reply) => {
-                if (err) {
-                    console.error(`Error deleting key ${key}: ${err.message}`);
-                    return reject(err);
-                }
-                resolve(reply);
-            });
-        });
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err, reply) => {
+        if (err) {
+          console.error(`Error setting key ${key}: ${err.message}`);
+          return reject(err);
+        }
+        resolve(reply);
+      });
     }
+    );
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) {
+          console.error(`Error deleting key ${key}: ${err.message}`);
+          return reject(err);
+        }
+        resolve(reply);
+      });
+    });
+  }
 }
 
 const redisClient = new RedisClient();
